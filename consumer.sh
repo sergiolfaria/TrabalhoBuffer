5552eac8ec90b66e849a698fd6a6e9a9af90458b
olzmooraes <luizotaviomoraesdacruz@gmail.com>
2022-10-25 21:33:16 -0300

    atualizacao luiz otavio

--- consumer.sh
+++ consumer.sh
@@ -1,51 +1,59 @@
-readvalue(){
+declare -i contador_log=1
+
+readvalue() {
     local value=$(cat $1)
     echo $value
 }
 
-checkBuffer(){
+checkBuffer() {
     declare -i buffer=$(readvalue buffer.txt)
     echo "buffer" $buffer "bufferSize" $bufferSize
     if [ "$buffer" -eq "0" ]; then
-        echo "0" > consumer.txt
+        echo "0" >consumer.txt
         local consumer=$(readvalue consumer.txt)
         while [ "$consumer" -eq "0" ]; do
-            echo "Waiting for consumer"
+            echo "Waiting for producer"
             sleep 1
             consumer=$(readvalue consumer.txt)
         done
     fi
 }
- 
+
 local consumer=$(readvalue consumer.txt)
 if [ "$consumer" -eq "0" ]; then
-    echo "1" > consumer.txt
+    echo "1" >consumer.txt
 fi
 
-function wakeUpProducer(){
+function wakeUpProducer() {
     local Producer=$(readvalue producer.txt)
     if [ "$producer" -eq "0" ]; then
-        echo "1" > producer.txt
+        echo "1" >producer.txt
     fi
 }
-i=1
-while true;do
-    checkBuffer
-    declare -i buffer=$(readvalue buffer.txt)
 
-    for ((i = 0; i == 0; i++)); do
+for ((i = 0; i == 0; i++)); do
     if [[ -e log.txt ]]; then
         rm -r log.txt
     fi
-    done
-    touch log.txt
+done
+sleep 10s
+touch log.txt
+
+while true; do
+
+    checkBuffer
+    declare -i buffer=$(readvalue buffer.txt)
+
     hora=$(date +"%T")
-    var_string_log=$(grep -i "nome" ./log/${i}.txt)
+    var_string_log=$(grep -i "nome" ./log/${contador_log}.txt)
     read -a vetor_string_log <<<$var_string_log
     line="${vetor_string_log[1]} ${vetor_string_log[2]} ${vetor_string_log[3]} $hora"
     echo $line >> log.txt
-    rm "./log/${i}.txt"
-    i=$(($i + 1))
+    rm "./log/${contador_log}.txt"
+    contador_log=$(($contador_log + 1))
+    if [ "$buffer" -eq "0" ]; then
+        wakeUpProducer
+    fi
 
-    echo "$(($buffer-1))" > buffer.txt
+    echo "$(($buffer - 1))" >buffer.txt
 done
